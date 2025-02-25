ed7bf8abf0c6dc021acb7de2cd428f43eafb5fa3
Klava147 <123240109@student.upnyk.ac.id>
2025-02-25 16:35:00 +0700

    Revert "tambahan hehe"
    This reverts commit 5c449bc2f2023272d0e958a710782db1e073e1d2.

--- tes.cpp
+++ tes.cpp
@@ -3,13 +3,5 @@ using namespace std;
 
 main(){
     cout << "Hello world" << endl;
-    cout << "hehe" << endl;
-    cout << "haha" << endl;
-    cout << "hihi" << endl;
-
-    cout << "bang ajarin bang" << endl;
-    cout << "coba" << endl;
-    cout << "tes" << endl;
-
-    cout << "tes pull" << endl;
+    cout << "hehe";
 }
